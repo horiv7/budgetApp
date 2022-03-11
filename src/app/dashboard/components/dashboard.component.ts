9b1f71ed7f8d8ad2f2142fb84b0fdd627089fcb8
unknown <aquamina2020@gmail.com>
2022-03-11 18:08:32 +0100

    add currency pipe to dashboard

--- src/app/dashboard/components/dashboard.component.ts
+++ src/app/dashboard/components/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ba-dashboard',
@@ -6,5 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  total: number = 254;
   constructor() {}
 }
